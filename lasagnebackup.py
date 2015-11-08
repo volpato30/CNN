badf90ba401807730ceea7e70b1ddf744fbc5f24
睿 乔 <volpato30@gmail.com>
2015-11-08 16:52:19 -0500

    using lstm to S&P index data

--- lasagnebackup.py
+++ lasagnebackup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 """
-Usage example employing Lasagne for digit recognition using the MNIST dataset.
-
 This example is deliberately structured as a long flat file, focusing on how
 to use Lasagne, instead of focusing on writing maximally modular and reusable
 code. It is used as the foundation for the introductory Lasagne tutorial:
@@ -41,42 +39,6 @@ def load_data():
 
     rval = [train_set,valid_set, test_set]
     return rval
-
-Conv2DLayer = lasagne.layers.Conv2DDNNLayer
-
-def inception_module(l_in, num_1x1, reduce_3x3, num_3x3, reduce_5x5, num_5x5, gain=1.0, bias=0.1):
-    """
-    inception module (without the 3x3s1 pooling and projection because that's difficult in Theano right now)
-    """
-    shape = l_in.get_output_shape()
-    out_layers = []
-
-    # 1x1
-    if num_1x1 > 0:
-        l_1x1 = lasagne.layers.NINLayer(l_in, num_units=num_1x1, W=lasagne.init.Orthogonal(gain), b=lasagne.init.Constant(bias))
-        out_layers.append(l_1x1)
-    
-    # 3x3
-    if num_3x3 > 0:
-        if reduce_3x3 > 0:
-            l_reduce_3x3 = lasagne.layers.NINLayer(l_in, num_units=reduce_3x3, W=lasagne.init.Orthogonal(gain), b=lasagne.init.Constant(bias))
-        else:
-            l_reduce_3x3 = l_in
-        l_3x3 = Conv2DLayer(l_reduce_3x3, num_filters=num_3x3, filter_size=(3, 3), border_mode="same", W=lasagne.init.Orthogonal(gain), b=lasagne.init.Constant(bias))
-        out_layers.append(l_3x3)
-    
-    # 5x5
-    if num_5x5 > 0:
-        if reduce_5x5 > 0:
-            l_reduce_5x5 = lasagne.layers.NINLayer(l_in, num_units=reduce_5x5, W=lasagne.init.Orthogonal(gain), b=lasagne.init.Constant(bias))
-        else:
-            l_reduce_5x5 = l_in
-        l_5x5 = Conv2DLayer(l_reduce_5x5, num_filters=num_5x5, filter_size=(5, 5), border_mode="same", W=lasagne.init.Orthogonal(gain), b=lasagne.init.Constant(bias))
-        out_layers.append(l_5x5)
-    
-    # stack
-    l_out = lasagne.layers.concat(out_layers)
-    return l_out
 
 
 def build_cnn(input_var=None):
