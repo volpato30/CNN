63a409c0ad50cc56f9ffe84144c731614e11763d
睿 乔 <volpato30@gmail.com>
2016-04-25 17:28:39 -0400

    okcoin

---
+++ bitcoin/dp.py
@@ -0,0 +1,22 @@
+#!/opt/sharcnet/python/2.7.5/gcc/bin/python
+
+import glob
+import scipy.io as sio
+import pandas as pd
+import numpy as np
+import os
+import re
+
+os.chdir('/scratch/rqiao/okcoin/2016-01')
+fname=glob.glob('*.mat')
+
+data=np.zeros((1,80),dtype=np.float32)
+time = np.zeros(1,dtype = np.float64)
+for file in fname:
+    temp=sio.loadmat(file)
+    data=np.concatenate((data,temp['orderbook']),axis=0)
+    time=np.concatenate((time,temp['timestamp']),axis=0)
+data = data[1:,:]
+time = time[1:]
+
+np.savez("/scratch/rqiao/okcoin/2016-01.npz",data,time)
