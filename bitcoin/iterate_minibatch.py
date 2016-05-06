1ff70806ec4f3439bae16225e656be2cb1ac057a
睿 乔 <volpato30@gmail.com>
2016-05-06 11:38:18 -0400

    fix

--- bitcoin/iterate_minibatch.py
+++ bitcoin/iterate_minibatch.py
@@ -2,7 +2,7 @@ import numpy as np
 def iterate_minibatches(inputs, targets, batchsize, window):
     assert len(inputs) == len(targets)
     for start_idx in range(0, len(inputs) - window - batchsize + 2, batchsize):
-        batch = np.zeros((batchsize,window,80),dtype = np.float32)
+        batch = np.zeros((batchsize,window,20),dtype = np.float32)
         label = np.zeros(batchsize,dtype = np.int8)
         for i in range(batchsize):
             batch[i] = inputs[(start_idx+i):(start_idx+i+window),:]
