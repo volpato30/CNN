b8bc5520fd9cec0a3fec22c85a1cd9adcf5f2af6
Rui Qiao <rqiao@mos-login.mosaic.sharcnet>
2016-05-29 20:31:41 -0400

    ss

--- futureHF/grid_search.py
+++ futureHF/grid_search.py
@@ -136,7 +136,7 @@ class MyAlgo(PairAlgoWrapper):
     def on_daystart(self, info):
         pass
 
-pair = ['m1505', 'm1509']
+pair = ['cs1505', 'cs1509']
 date_list = get_trade_day(pair)
 algo = { 'class': MyAlgo }
 algo['param'] = {'x': pair[0],
@@ -174,4 +174,4 @@ result = pd.DataFrame({"rolling": [p[0] for p in pars],
                        "sd_coef": [p[2] for p in pars],
                        "stop_win": [p[3] for p in pars],
                        "PNL": [float(f) for f in final_profit]})
-pickle.dump(result, open( "m_stopwin_result.p", "wb" ) )
+pickle.dump(result, open( "cs_stopwin_result.p", "wb" ) )
