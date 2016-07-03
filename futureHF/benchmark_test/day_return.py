5a4528ed2ebc630bf4f79f520cf84b279e3a7bca
睿 乔 <volpato30@gmail.com>
2016-07-03 13:45:12 -0400

    ni

--- futureHF/benchmark_test/day_return.py
+++ futureHF/benchmark_test/day_return.py
@@ -201,7 +201,7 @@ def run_simulation(param, date_list, product):
 date_list = [str(x).split(' ')[0] for x in pd.date_range('2015-01-01','2016-03-31').tolist()]
 roll_list = np.arange(1000, 11000, 1000)
 sd_list = np.arange(0.5, 4.1, 0.25)
-product_list = ['al', 'ag', 'au', 'cu', 'pb', 'ru', 'zn', 'ni']
+product_list = ['ni']
 pars = list(itertools.product(roll_list, sd_list))
 num_cores = 20
 for product in product_list:
